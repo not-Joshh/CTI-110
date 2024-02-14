f0387d31de4eb07b01041c266aefbac6f2d98b40
not-Joshh <160041271+not-Joshh@users.noreply.github.com>
2024-02-14 10:35:57 -0500

    First python upload

---
+++ P1LAB1_Murray_Joshua.py
@@ -0,0 +1,11 @@
+#Joshua Murray
+#2/12/2024
+#P1LAB1
+#using input and output
+
+#create a variable to hold first name
+first_name = input ("enter your first name: ")
+
+#print first_name with a greeting
+print ("Hey", first_name)
+print("welcome to zyBooks!")
