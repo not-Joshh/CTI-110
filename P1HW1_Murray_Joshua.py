f0e78ae1577d1197befc1c70810812380cc302f8
not-Joshh <160041271+not-Joshh@users.noreply.github.com>
2024-02-19 11:27:47 -0500

    Add files via upload

---
+++ P1HW1_Murray_Joshua.py
@@ -0,0 +1,15 @@
+#Joshua Murray
+#02/19/2024
+#P1HW1 - Input and Output
+
+num1 = int(input("Enter Integer: "))
+print (num1)
+
+print (num1, "squared is", num1**2)
+print (num1, "cubed is", num1**3)
+num2 = int(input("Enter another integer: "))
+print (num2)
+print (num1, "+", num2, "is", num1+num2)
+
+print (num1, "*", num2, "is", num1*num2)
+       
