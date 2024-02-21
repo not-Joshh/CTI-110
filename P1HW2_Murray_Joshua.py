fbe14cbeba819fecf204a44865ba561b5b3f4a9b
not-Joshh <160041271+not-Joshh@users.noreply.github.com>
2024-02-21 12:28:18 -0500

    Add files via upload

---
+++ P1HW2_Murray_Joshua.py
@@ -0,0 +1,30 @@
+#Joshua Murray
+#2/21/2024
+#P1HW2
+
+#Getting integers from user
+
+budget = int(input("Enter your budget: "))
+
+destination = input("Enter your destination: ")
+
+gas = float(input("How much do you think will be spent on gas?"))
+
+hotel = float(input("How much will you need for a hotel?"))
+
+food = float(input("Last, how much will you need for food?"))
+
+expenses = gas+hotel+food
+balance = budget-expenses
+
+print ("/n------Travel Expenses------")
+
+print ("Location: ", destination)
+
+print ("Initial Budget: ", budget)
+
+print ("Fuel:", gas)
+print ("Accomodation:", hotel)
+print ("Food:", food)
+
+print ("Remaining Balance:", balance)    
