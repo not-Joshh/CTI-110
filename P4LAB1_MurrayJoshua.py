e054e67a4d191b7daffd576b9857bdcd65b98e34
not-Joshh <160041271+not-Joshh@users.noreply.github.com>
2024-04-10 10:16:57 -0400

    Add files via upload

---
+++ P4LAB1_MurrayJoshua.py
@@ -0,0 +1,23 @@
+#Joshua Murray
+#P4LAB1
+#3/25/2024
+
+
+import turtle
+win = turtle.Screen()
+tim = turtle.Turtle()
+
+
+tim.pensize(4)
+tim.pencolor("blue")
+tim.shape("turtle")
+
+
+for item in range(4):
+    tim.forward(100)
+    tim.left(90)
+
+    
+for item in range(3):
+    tim.forward(100)
+    tim.left(120)
